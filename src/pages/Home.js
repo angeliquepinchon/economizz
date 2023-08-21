69d3ab503443d64a8d7ba97a2d1c7a23b17d1a4d
angelique pinchon <ange.pinchon@gmail.com>
2023-08-21 16:08:27 +0200

    feature/home componant created

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "../componants/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <h1 className="container-title">Calcule tes économies</h1>
+        <section>
+          <h2>InputSection</h2>
+          <div>
+            <input type="text" placeholder="prix init" />
+            <input type="text" placeholder="solde" />
+            <div>
+              <h2>pastilles</h2>
+            </div>
+            <input type="text" placeholder="prix remisé" />
+            <p> tu economises 5€</p>
+            <div>
+              <h2>photo</h2>
+            </div>
+            <button>Enregistrer</button>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
