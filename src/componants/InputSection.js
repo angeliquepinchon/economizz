9d4dc5244ac0fba19f38988769a3bbeb30f7a6d0
angelique pinchon <ange.pinchon@gmail.com>
2023-08-28 16:52:36 +0200

    handle discountePills

--- src/componants/InputSection.js
+++ src/componants/InputSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import DiscountInput from "./DiscountInput";
 import PriceInput from "./PriceInput";
 import DiscountPill from "./DiscountPill";
@@ -7,17 +7,25 @@ import DiscountedPrice from "./DiscountedPrice";
 const InputSection = () => {
   const NUMBER_PILLS = 8;
   const discountPills = [];
+  const [selectedDiscount, setSelectedDiscount] = useState(0);
 
-  for (let i = 1; i < NUMBER_PILLS; i++) {
-    discountPills.push(<DiscountPill key={i} />);
+  const handleDiscounteClick = (percentage) => {
+    setSelectedDiscount(percentage);
+    console.log(percentage);
+  };
+
+  for (let i = 1; i <= NUMBER_PILLS; i++) {
+    discountPills.push(
+      <DiscountPill key={i} index={i * 10} onClick={handleDiscounteClick} />
+    );
   }
 
   return (
     <div>
       <PriceInput />
-      <DiscountInput />
+      <DiscountInput selectedDiscount={selectedDiscount} />
       {discountPills}
-      <DiscountedPrice />
+      <DiscountedPrice selectedDiscount={selectedDiscount} />
     </div>
   );
 };
